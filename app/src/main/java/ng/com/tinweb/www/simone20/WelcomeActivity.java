2025bc45a16c12f948495bab17879c78b4c62405
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-08-24 13:14:31 +0100

    Initial Commit

---
+++ app/src/main/java/ng/com/tinweb/www/simone20/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package ng.com.tinweb.www.simone20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
