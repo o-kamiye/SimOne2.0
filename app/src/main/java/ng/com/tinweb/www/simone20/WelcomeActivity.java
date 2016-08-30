005d74de03f7ec5d7252f65926721ad4626737b8
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@andela.com>
2016-08-30 13:09:55 +0100

    [#128938785] Complete full design of launcher activity

--- app/src/main/java/ng/com/tinweb/www/simone20/WelcomeActivity.java
+++ app/src/main/java/ng/com/tinweb/www/simone20/WelcomeActivity.java
@@ -1,13 +1,18 @@
 package ng.com.tinweb.www.simone20;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ng.com.tinweb.www.simone20.databinding.ActivityWelcomeBinding;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ActivityWelcomeBinding activityWelcomeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        activityWelcomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
     }
 }
