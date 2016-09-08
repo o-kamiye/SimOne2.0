7f66fbd2b7bbcedb0fceecc2ecd494f2505bf0cb
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-09-08 03:04:07 +0100

    [#128938807] Implement creating and updating Reminders in the database table

--- app/src/main/java/ng/com/tinweb/www/simone20/data/DataStore.java
+++ app/src/main/java/ng/com/tinweb/www/simone20/data/DataStore.java
@@ -2,6 +2,8 @@ package ng.com.tinweb.www.simone20.data;
 
 import android.database.Cursor;
 
+import java.util.List;
+
 /**
  * Created by kamiye on 08/09/2016.
  */
@@ -9,12 +11,15 @@ public interface DataStore {
 
     boolean save(String contactId, String contactName, int interval);
     boolean update(String contactId, String contactName, int interval);
-    void get(String contactId, ActionCallback callback);
+    void getSingle(String contactId, ActionCallback callback);
+    void getMultiple(ActionCallback callback);
     boolean delete(String contactId);
 
     interface ActionCallback {
         void onGetSuccess(String contactId, String contactname, int interval);
         void onGetError();
+        List<Reminder> onGetMultipleSuccess();
+        void onGetMultipleError();
     }
 
 }
