e3da7d683fb8c2a902318a36c418d32d5ca7fc91
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-09-12 17:07:58 +0100

    [#128938975] Add grouppresenter and groupUI test

---
+++ app/src/test/java/ng/com/tinweb/www/simone20/group/GroupPresenterTest.java
@@ -0,0 +1,43 @@
+package ng.com.tinweb.www.simone20.group;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by kamiye on 12/09/2016.
+ */
+public class GroupPresenterTest {
+
+    @Mock
+    private IGroupView groupView;
+
+    private GroupPresenter groupPresenter;
+
+    @Before
+    public void setUpTest() {
+        MockitoAnnotations.initMocks(this);
+        groupPresenter = new GroupPresenter(groupView);
+    }
+
+    @Test
+    public void testSetGroupCount() {
+        groupPresenter.setTotalGroupsCount();
+
+        verify(groupView).setGroupsCountTextView(2);
+    }
+
+    @Test
+    public void testEditGroup() {
+        // TODO add edit group action here when implemented
+    }
+
+    @Test
+    public void testDeleteGroup() {
+        // TODO add delete group action here when implemented
+    }
+
+}
